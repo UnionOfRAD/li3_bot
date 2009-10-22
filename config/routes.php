46919875dfc18deccea26d527548e5cb6ba0e1b6
gwoo <gwoohoo@gmail.com>
2009-10-22 04:47:24 -0700

    updating routes

--- config/routes.php
+++ config/routes.php
@@ -2,9 +2,9 @@
 
 use \lithium\http\Router;
 
-Router::connect('/bot', array('plugin' => 'lithium_bot', 'controller' => 'logs'));
+Router::connect('/bot', array('plugin' => 'li3_bot', 'controller' => 'logs'));
 Router::connect('/bot/{:library}/{:args}', array(
-	'plugin' => 'lithium_bot', 'controller' => 'logs', 'action' => 'view'
+	'plugin' => 'li3_bot', 'controller' => 'logs', 'action' => 'view'
 ));
 
 ?>
