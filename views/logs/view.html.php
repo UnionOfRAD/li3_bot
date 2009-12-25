1cba0f8472e9d3c2826bc581621731579232190d
Niel Archer <niel.archer@blueyonder.co.uk>
2009-12-25 04:24:37 +0000

    Adding anchor wrappers around links in messages.
    Signed-off-by: David Persson <davidpersson@gmx.de>

--- views/logs/view.html.php
+++ views/logs/view.html.php
@@ -4,11 +4,16 @@
 		$hash = abs(crc32($line['user']));
 		$rgb = array($hash % 255, $hash % 255, $hash % 255);
 		$rgb[$hash % 2] = 0;
+		$message = preg_replace(
+			'@(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)@',
+			'<a href="$1">$1</a>',
+			$h($line['message'])
+		);
 	?>
  	<tr>
 		<td class="time"><?=$line['time'];?></td>
 		<td class="user" style="color: rgb(<?=implode(',' , $rgb)?>);"><?=$line['user']?></td>
-		<td class="message"><?= $h($line['message']); ?></td>
+		<td class="message"><?php echo $message; ?></td>
 	</tr>
 <?php endforeach; ?>
 </table>
