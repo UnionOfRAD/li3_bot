a77b15fb37480a1d4927918681f7bffc5e0a71a7
David Persson <davidpersson@gmx.de>
2011-01-20 20:02:13 +0100

    Updating template for non existing tells, channels and karma.

--- views/pages/home.html.php
+++ views/pages/home.html.php
@@ -1,42 +1,54 @@
 <div class="logs">
 	<h3>Channel Logs</h3>
-	<ul class="channels">
-		<?php foreach ((array) $channels as $channel): ?>
-			 <li><?=$this->html->link("#{$channel}", array(
-					'library' => 'li3_bot',
-					'controller' => 'logs', 'action' => 'index',
-			) + compact('channel')); ?></li>
-		<?php endforeach; ?>
-	</ul>
+	<?php if ($channels): ?>
+		<ul class="channels">
+			<?php foreach ((array) $channels as $channel): ?>
+				 <li><?php echo $this->html->link("#{$channel}", array(
+						'library' => 'li3_bot',
+						'controller' => 'logs', 'action' => 'index',
+				) + compact('channel')); ?></li>
+			<?php endforeach; ?>
+		</ul>
+	<?php else: ?>
+		No channel logs have yet been created.
+	<?php endif; ?>
 </div>
 
 <div class="tells">
 	<h3>Most Recent Tells</h3>
-	<dl class="tells">
-	<?php foreach ($tells as $key => $value): ?>
-		<dt><?php echo $key ?></dt><dd><?php echo $value ?></dd>
-	<?php endforeach; ?>
-	</dl>
-	<?=$this->html->link('View all tells...', array(
-		'library' => 'li3_bot',
-		'controller' => 'tells', 'action' => 'index'
-	)); ?>
+	<?php if ($tells): ?>
+		<dl class="tells">
+		<?php foreach ($tells as $key => $value): ?>
+			<dt><?php echo $key ?></dt><dd><?php echo $value ?></dd>
+		<?php endforeach; ?>
+		</dl>
+		<?php echo $this->html->link('View all tells...', array(
+			'library' => 'li3_bot',
+			'controller' => 'tells', 'action' => 'index'
+		)); ?>
+	<?php else: ?>
+		No tells, yet.
+	<?php endif; ?>
 </div>
 
 <div class="karmas">
 	<h3>Karma Highscore</h3>
-	<table class="karma">
-		<tr>
-			<th>score</th>
-			<th>nick</th>
-		</tr>
-	<?php foreach ($karmas as $key => $value): ?>
-		<tr>
-			<td><?php echo $value ?></td>
-			<td><?php echo $key ?></td>
-		</tr>
-	<?php endforeach; ?>
-	</table>
+	<?php if ($karmas): ?>
+		<table class="karma">
+			<tr>
+				<th>score</th>
+				<th>nick</th>
+			</tr>
+		<?php foreach ($karmas as $key => $value): ?>
+			<tr>
+				<td><?php echo $value ?></td>
+				<td><?php echo $key ?></td>
+			</tr>
+		<?php endforeach; ?>
+		</table>
+	<?php else: ?>
+		No karma data.
+	<?php endif; ?>
 </div>
 
 <div class="plugins">
